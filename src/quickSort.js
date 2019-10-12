5bf67a1d9b9add770521e2357a057edf8c52433c
Ha Nam <namnguyendangha@gmail.com>
2019-10-12 16:28:45 +0700

    code2

--- src/quickSort.js
+++ src/quickSort.js
@@ -5,35 +5,24 @@ function swap(left, right) {
 }
 
 function partition(items, left, right) {
-  var pivot = items[Math.floor((right + left) / 2)],
-    i = left,
-    j = right;
-  while (i <= j) {
-    while (items[i] < pivot) {
-      i++;
-    }
-    while (items[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      swap(items, i, j);
-      i++;
-      j--;
+  var i = start + 1;
+  var pivot = items[Math.floor(left)];
+  for(let j = left + 1; j <= right; j++){
+    if(items[j] < pivot){
+      swap(items[i], items[j]);
+      i+=1
     }
   }
-  return i;
+  swap(items[left], items[i - 1]);
+  return i - 1;
 }
 
 export function quickSort(items, left, right) {
   var index;
-  if (items.length > 1) {
+  if (left < right) {
     index = partition(items, left, right);
-    if (left < index - 1) {
-      quickSort(items, left, index - 1);
-    }
-    if (index < right) {
-      quickSort(items, index, right);
-    }
+    quickSort(items, left, index - 1);
+    quickSort(items, index + 1, right);
   }
   return items;
 }
