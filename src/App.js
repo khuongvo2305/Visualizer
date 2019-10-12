2916baac2972383bb7c5850c62ea8c1ae19ff5d9
Max Phan <max@vinova.sg>
2019-10-12 15:10:57 +0700

    add sort function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-
+import { quickSort } from "./quickSort";
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
@@ -33,6 +33,7 @@ export default function CustomizedInputs() {
   const classes = useStyles();
   const [size, setSize] = useState(0);
   const [arr, setArr] = useState()
+  const [resulf, setResulf] = useState()
   const handleClick = () => {
     if(size > 0){
       setArr(Array.from({length: size}, () => Math.floor(Math.random() * size)))
@@ -43,12 +44,32 @@ export default function CustomizedInputs() {
   const onChange = e => {
     setSize(e.target.value)
   }
+  const handleSort = () => {
+    if(arr && arr.length > 0){
+      let arrNumber = [...arr];
+      setResulf(quickSort(arrNumber, 0, arr.length - 1))
+    }
+  }
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           {
-            arr && arr.map(number => number + ' | ')
+            arr &&
+            <React.Fragment>
+              <label>Array:</label><br/>
+              {arr.map(number => number + ' | ')}
+            </React.Fragment>
+          }
+        </Grid>
+        <Grid item xs={12}>
+          {
+
+            resulf &&
+              <React.Fragment>
+                <label>Resulf:</label><br/>
+                {resulf.map(number => number + ' | ')}
+              </React.Fragment>
           }
         </Grid>
         <Grid item xs={12}>
@@ -69,7 +90,7 @@ export default function CustomizedInputs() {
               <Button variant="contained" onClick={handleClick} color="primary" className={classes.button}>
                 Random Array
               </Button>
-              <Button variant="contained" color="primary" className={classes.button}>
+              <Button variant="contained" onClick={handleSort} color="primary" className={classes.button}>
                 Sort
               </Button>
             </form>
