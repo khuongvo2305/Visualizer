d104d24a3bc5de4ce8313ceb369e75574b98d0bd
sy.nghiem <sy.nghiem@codebox.vn>
2019-10-12 15:20:57 +0700

    add button example code

--- src/App.js
+++ src/App.js
@@ -81,6 +81,25 @@ export default function CustomizedInputs() {
           </Paper>
         </Grid>
       </Grid>
+      <Grid container>
+      <Button variant="contained" color="primary" className={classes.button} id="example-code-btn">
+        Example code
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </Grid>
     </div>
   );
 }
