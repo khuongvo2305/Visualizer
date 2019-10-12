d3dd26900200760c8c0d503fc51ab28d84f0cc7f
son <son.doan993@hcmut.edu.vn>
2019-10-12 16:28:45 +0700

    array style and show pseudo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -34,10 +39,10 @@ export default function CustomizedInputs() {
   const [size, setSize] = useState(0);
   const [arr, setArr] = useState()
   const handleClick = () => {
-    if(size > 0){
+    if(size > 0 && size <= 1000 ){
       setArr(Array.from({length: size}, () => Math.floor(Math.random() * size)))
     } else {
-      alert("Please select size!")
+      alert("Please select valid size! ( from 0 to 1000 )")
     }
   }
   const onChange = e => {
@@ -45,24 +50,34 @@ export default function CustomizedInputs() {
   }
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} style = {{textAlign:"center"}}>
         <Grid item xs={12}>
           {
-            arr && arr.map(number => number + ' | ')
-          }
+            arr && arr.map((number, index) => {
+              return (
+                  <li className="waves-effect" style={{width:"60px",height:number+"0px",fontSize:"20px",color:"white", textAlign:"center",backgroundColor:"grey",border:"2px",borderStyle:"double",float:
+                "left", listStyleType:"none"}}
+                  key ={index}
+                  id= {number}
+                  >
+                      {number}
+                  </li>
+              );
+          })}
         </Grid>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
             <form noValidate autoComplete="off">
-              <TextField
+              <TextField 
                 id="standard-number"
                 label="Size"
                 type="number"
+                
                 className={classes.textField}
                 InputLabelProps={{
                   shrink: true,
                 }}
-                InputProps={{ inputProps: { min: 0 } }}
+                InputProps={{ inputProps: { min: 0, max: 1000 } }}
                 margin="normal"
                 onChange={onChange}
               />
@@ -75,6 +90,28 @@ export default function CustomizedInputs() {
             </form>
           </Paper>
         </Grid>
+      </Grid>
+      <Grid item xs={12} style = {{height:"30%"}}>
+        <ExpansionPanel style={{width:"30%", marginLeft: 20, marginTop: 20}}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            
+            <Typography className={classes.heading}>Pseudocode</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              <b>quickSort(arr[], low, high):</b>
+                <p>{"\u00a0 \u00a0 if (low < high):"}</p>
+                <p>{"\u00a0 \u00a0 \u00a0 pivot = partition(arr, low, high)"}</p>
+                <p>{"         \u00a0 \u00a0 \u00a0 quickSort(arr, low, pivot - 1)"}</p>
+                <p>{"        \u00a0 \u00a0 \u00a0 quickSort(arr, pivot + 1, high)"}</p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        
       </Grid>
     </div>
   );
